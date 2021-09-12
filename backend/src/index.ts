866f8081e5ba7e3c2e6500ffc1bf44888ad767e8
Eric Schmidt <eric.schmidt.blue@gmail.com>
2021-09-11 21:58:22 -0500

    initial setup of dependencies and test route

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
